6384d027e749c870d9b560506f5b2307bb0e2ddc
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-06-10 22:15:14 +0100

    set up linters in local env

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
