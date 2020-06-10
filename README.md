e413636a5a2dfe81f613181e53ffb6253bc083ad
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-06-10 18:15:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Snip-Text MarkDown
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
